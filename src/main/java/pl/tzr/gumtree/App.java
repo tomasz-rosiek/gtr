87b5c90b9ad8755f316947da23ca1ddda99cf7fb
Tomasz Rosiek <tomasz.rosiek@ocado.com>
2015-07-02 20:56:15 +0100

    Added support for building JAR with bundled app and the script to run it.

--- src/main/java/pl/tzr/gumtree/App.java
+++ src/main/java/pl/tzr/gumtree/App.java
@@ -9,6 +9,6 @@ public class App {
 
         System.out.println("There are " + addressBook.maleCount() + " males in the address book.");
         System.out.println("The oldest person in the address book is " + addressBook.oldestPerson().get() + ".");
-        System.out.println("Bill is older than Paul by " + addressBook.calculateAgeDifferenceInDays("Bill McKnight", "Paul Robinson"));
+        System.out.println("Bill is older than Paul by " + addressBook.calculateAgeDifferenceInDays("Bill McKnight", "Paul Robinson") + " days.");
     }
 }
