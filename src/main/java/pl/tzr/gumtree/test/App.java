76533f75614d0f81b6771ad76e5071461b027154
Tomasz Rosiek <tomasz.rosiek@ocado.com>
2015-07-02 18:28:15 +0100

    Initial commit of the empty maven project created from archetype

---
+++ src/main/java/pl/tzr/gumtree/test/App.java
@@ -0,0 +1,13 @@
+package pl.tzr.gumtree.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
